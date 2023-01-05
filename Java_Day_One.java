a91ae601b30ede5fa36a15e3a4d0dbeb191209f3
김명진 <80269927+codenerd-from-alien@users.noreply.github.com>
2023-01-05 14:23:46 +0900

    자바 깃 으로 올리려함
    자바 파일 깃으로 연습으로 올렸지만 파일이 안올라와서 다르게 직접 업로드함

---
+++ Java_Day_One.java
@@ -0,0 +1,12 @@
+package Java_Practice_pack;
+
+public class Java_Day_One {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi, Yo");
+		System.out.println("changes");
+		System.out.println("new, Branch");
+	}
+
+}
