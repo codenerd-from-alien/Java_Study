ca61b652a464c27070abf289f2af7437405c84ae
김명진 <80269927+codenerd-from-alien@users.noreply.github.com>
2023-01-08 00:38:59 +0900

    Update Java_paek_joon_15596.java
    자바 코드 잘못적어서 다시 업로드

--- Java_paek_joon_15596.java
+++ Java_paek_joon_15596.java
@@ -1,5 +1,19 @@
 package Java_Practice_pack;
 
+class Test{
+	public long sum(int[] a) {
+		long sum = 0;
+		for(int i =0; i < a.length; i++) {
+			sum += a[i];
+		}
+		
+		return sum;
+	}
+}
+
+
+
+
 public class Java_paek_joon_15596 {
 
 	public static void main(String[] args) {
