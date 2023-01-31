6859547ddf5e4f9cd33ee83ae8b592458e1b7b2f
김명진 <80269927+codenerd-from-alien@users.noreply.github.com>
2023-01-31 22:59:04 +0900

    add from datastructure file in repo file
    First day of data Structure

---
+++ Array.java
@@ -0,0 +1,19 @@
+package datastructurefile;
+
+public class Array {
+
+	public void arrayDemo() {
+		int []myArray = new int[5];
+		myArray[0] = 5;
+		myArray[1] = 1;
+		myArray[2] = 8;
+		myArray[3] = 2;
+		myArray[4] =10;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Array Data Structure
+	}
+
+}
