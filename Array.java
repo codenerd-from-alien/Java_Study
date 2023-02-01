6d1904f8ec99470d773a22b8c9869cb57946ffc0
김명진 <80269927+codenerd-from-alien@users.noreply.github.com>
2023-02-02 00:15:07 +0900

    Update Array.java
    I tried to change the code of removing odd numbers among Arrays when I get it as parameter. However I have no time for changing more and  adding up the code line.
    When i change this code of line next time. I got to change it!

--- Array.java
+++ Array.java
@@ -9,11 +9,37 @@ public class Array {
 		myArray[2] = 8;
 		myArray[3] = 2;
 		myArray[4] =10;
+		print(myArray);
+	}
+	
+	public void print(int arr[]) {
+		int n = arr.length;
+		for(int i =0; i < n; i++) {
+			System.out.print(arr[i] + " ");
+			System.out.println();
+		}
+	}
+	
+	public void remove_even_integer(int arr[]) {
+		//List<Integer> lst = Arrays.asList(arr);자바 배열을 매개변수로 받았을 때에 리스트형으로 변환을 하는 방법을 찾아 봐야함
+		
+		for(int i =0; i < lst.size(); i++) {
+			
+			if(lst.get(i) % 2==0) {
+				
+			}
+		}
+		
+		
+		
 	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// Array Data Structure
+		Array arr = new Array();
+		arr.arrayDemo();		
 	}
+
 
 }
