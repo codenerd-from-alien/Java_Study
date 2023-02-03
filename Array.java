c9a056d8d7f07352348bb5cb160f2b9259949851
김명진 <80269927+codenerd-from-alien@users.noreply.github.com>
2023-02-03 17:42:40 +0900

    Update Array.java
    Moving zeros to backs

--- Array.java
+++ Array.java
@@ -58,7 +58,19 @@ public class Array {
 		return min;
 	}
 	
-	
+	public void move_to_end(int arr[]) {
+		int j=0;
+		for(int i =0; i <arr.length; i++) {
+			if(arr[i] !=0 && arr[j] ==0) {
+				int temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+			if(arr[j] !=0) {
+				j++;
+			}
+		}
+	}
 	
 	
 	public static void print_rvs_array(int numbers[], int start, int end) {
